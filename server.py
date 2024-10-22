95b47e22159e0fa31a9526a1701024cff4e89b7e
999Marv <marvinsiri123@gmail.com>
2024-10-21 20:22:21 -0400

    rendered static blogs

--- server.py
+++ server.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template
-import random
-import datetime
+import requests
 
 app = Flask(__name__)
 
-@app.route('/hi')
-def home():
-    rand = random.randint(1,10)
-    current_year = datetime.now().year
-    return render_template('index.html', rand=rand, copy_right_year=current_year)
+def fetch_blogs():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    return response.json()
 
+def fetch_blog(number):
+    posts = fetch_blogs()
+
+    for post in posts:
+        if post["id"] == int(number):
+            return post
+    
+    return {"msg": "doesn't exist"}
+
+@app.route('/')
+def get_blogs():
+    return render_template("blogs.html", posts = fetch_blogs())
+
+@app.route('/<number>')
+def get_blog(number):
+    return render_template("blog.html", post = fetch_blog(number))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8000,debug=True)
